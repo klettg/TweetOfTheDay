c69620d377ec50e07964c4cc00a295bbc3fd1aa4
Griffin Klett <32664313+klettg@users.noreply.github.com>
2022-09-15 19:09:00 -0400

    commiting

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { TwitterTweetEmbed } from "react-twitter-embed";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Griffin, Nate, and Evan's Tweet of the Day</h1>
+      <h2>Wednesday, September 15th:</h2>
+      <TwitterTweetEmbed
+        tweetId={"933354946111705097"}
+        options={{ width: 550, align: "center" }}
+      />
+      <h2>Tuesday, September 14th:</h2>
+      <TwitterTweetEmbed
+        tweetId={"1570042112347099137"}
+        options={{ width: 550, align: "center" }}
+      />
+    </div>
+  );
+}
+
+export default App;
