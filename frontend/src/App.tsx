db347e9e98343f4d1042b2b417571db3d5f79cfa
Griffin Klett <32664313+klettg@users.noreply.github.com>
2022-09-15 19:38:42 -0400

    adding api call

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import "./App.css";
-import { TwitterTweetEmbed } from "react-twitter-embed";
+import Tweets from "./components/Tweets";
 
 function App() {
   return (
     <div className="App">
       <h1>Griffin, Nate, and Evan's Tweet of the Day</h1>
-      <h2>Wednesday, September 15th:</h2>
-      <TwitterTweetEmbed
-        tweetId={"933354946111705097"}
-        options={{ width: 550, align: "center" }}
-      />
-      <h2>Tuesday, September 14th:</h2>
-      <TwitterTweetEmbed
-        tweetId={"1570042112347099137"}
-        options={{ width: 550, align: "center" }}
-      />
+      <Tweets></Tweets>
     </div>
   );
 }
